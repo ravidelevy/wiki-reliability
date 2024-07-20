8d4683492be30b73c39902c6787d51b475fa82bc
Ravid Levy <ravid.levy@mail.huji.ac.il>
2024-07-20 18:16:05 +0300

    fix typos

--- visualisation.py
+++ visualisation.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 # todo choose one of both plot libraries
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 def data_labels_distribution():
     counts = {0: {}, 1: {}}
@@ -17,7 +16,7 @@ def data_labels_distribution():
     counts_df = pd.DataFrame(counts).fillna(0)
     counts_df.plot(kind='bar', figsize=(10, 6))
     plt.title('Number of True and False Labels for Each Unreliability Type')
-    plt.xlabel('Unreliablity')
+    plt.xlabel('Unreliability')
     plt.ylabel('Count')
     plt.tick_params(labelsize=6)
     plt.xticks(ticks=[i for i in range(10)], labels=[csv_path.split('_')[0] for csv_path in csv_paths], rotation=0)
