b0afabb39ba830376d2ac6bf5aeac5c7730aa40c
Ravid Levy <ravid.levy@mail.huji.ac.il>
2024-07-20 21:25:26 +0300

    finished scaling visualization

--- visualisation.py
+++ visualisation.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 
 # todo choose one of both plot libraries
@@ -23,6 +24,36 @@ def data_labels_distribution():
     plt.legend(title='has_template values')
     plt.savefig('data_labels_distribution.png')
 
+def data_attributes_scale():
+    csv_paths = os.listdir('data')
+    dataframes = []
+    for csv_path in csv_paths:
+        df = pd.read_csv(f"data/{csv_path}")
+        numerical_df = df.drop(columns=["article_quality_score"])
+        for column in numerical_df.columns:
+            df[column] = df[column].astype(np.float64)
+        
+        dataframes.append(df)
+    
+    unified_df = dataframes[0]
+    for df in dataframes[1:]:
+        unified_df = pd.merge(unified_df, df, how='outer')
+    
+    no_indexes_df = unified_df.drop(columns=["Unnamed: 0", "page_id", "revision_id",
+                                             "revision_id.key", "has_template"])
+    numerical_columms = no_indexes_df.select_dtypes(include=['number']).columns
+    numerical_df = no_indexes_df[numerical_columms]
+    plt.figure(figsize=(10, 6))
+    numerical_df.boxplot()
+    plt.yscale('log')
+    plt.title('Box Plot of the Wiki Reliability Dataset attributes')
+    plt.xticks(ticks=[i for i in range(1, 20)],
+               labels=[column.replace("_", "\n") for column in numerical_columms], rotation=0)
+    plt.tick_params(labelsize=5)
+    plt.xlabel('Attribute')
+    plt.ylabel('Values')
+    plt.savefig('data_attributes_scale.png')
+
 if __name__ == "__main__":
     data_labels_distribution()
-
+    data_attributes_scale()
