96558b5f1b8896fe9435323cba7e8eaddf2af7ac
Ravid Levy <ravid.levy@mail.huji.ac.il>
2024-09-21 18:10:47 +0300

    add tfidf files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import pandas as pd
 
-from tfidf import get_features, lookup
+from tfidf import get_features, lookup, unify_record
 from crawler import crawl, create_dataframes, load_jsons, save_dataframes, save_jsons
 from xgboost_classify import xgboost_classify
 
@@ -19,14 +19,10 @@ def unify_idf(original_data_dir, pages_dir, tfidf_dir, unified_idf_dir):
         new_rows = []
         for page in pages[dataset]:
             title = page["title"].lower().replace(" ", "-")
-            idf_record = wiki_idf[wiki_idf["token"] == title]
+            idf_record = lookup(wiki_idf, title)
             if idf_record.shape[0] > 0:
-                reliability_record = reliability_dataset[reliability_dataset["revision_id"] == page["revision_id"]]
-                new_record = reliability_record.to_dict(orient="records")[0]
-                idf_value = idf_record.to_dict(orient="records")[0]["idf"]
-                new_record["idf"] = idf_value
+                new_record = unify_record(reliability_dataset, idf_record, page["revision_id"])
                 new_rows.append(new_record)
-                break
         
         unified_datasets[dataset] = pd.DataFrame(new_rows)
     
--- src/tfidf.py
+++ src/tfidf.py
@@ -5,4 +5,11 @@ def get_features(directory, filename):
     return pd.read_csv(f"{directory}/{filename}")
 
 def lookup(dataframe, term):
-    return dataframe.loc[dataframe["token"] == term]
+    return dataframe[dataframe["token"] == term]
+
+def unify_record(reliability_dataset, idf_record, revision_id):
+    reliability_record = reliability_dataset[reliability_dataset["revision_id"] == revision_id]
+    new_record = reliability_record.to_dict(orient="records")[0]
+    idf_value = idf_record.to_dict(orient="records")[0]["idf"]
+    new_record["idf"] = idf_value
+    return new_record
