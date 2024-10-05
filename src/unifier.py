f657044e02e0cb2adc1d7a736ae27e1354e90b7b
Ravid Levy <ravid.levy@mail.huji.ac.il>
2024-10-06 00:16:57 +0300

    got pagerank data

--- src/unifier.py
+++ src/unifier.py
@@ -4,12 +4,11 @@ import pandas as pd
 def get_features(directory, filename):
     return pd.read_csv(f"{directory}/{filename}")
 
-def lookup(dataframe, term):
-    return dataframe[dataframe["token"] == term]
+def lookup(dataframe, term, key):
+    return dataframe[dataframe[key] == term]
 
-def unify_record(reliability_dataset, idf_record, revision_id):
+def unify_record(reliability_dataset, record, revision_id, value):
     reliability_record = reliability_dataset[reliability_dataset["revision_id"] == revision_id]
     new_record = reliability_record.to_dict(orient="records")[0]
-    idf_value = idf_record.to_dict(orient="records")[0]["idf"]
-    new_record["idf"] = idf_value
+    new_record[value] = record.to_dict(orient="records")[0][value]
     return new_record
